a7119e3554de77adb2337e4e32f419b8d1e3c131
gucan <gucancan@souche.com>
2020-04-15 10:36:33 +0800

    del muji

--- material-tpl/muji/block/src/components/IconFont/index.jsx
+++
@@ -1,8 +0,0 @@
-import { Icon } from 'antd';
-import setting from '@/setting';
-// 使用：
-// import IconFont from '@/components/IconFont';
-// <IconFont type='icon-demo' className='xxx-xxx' />
-export default Icon.createFromIconfontCN({
-  scriptUrl: setting.iconfontUrl,
-});
