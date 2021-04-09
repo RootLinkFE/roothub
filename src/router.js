83167b494e934b9ee5d5d77b68524d46cb2b6fcb
giscafer <giscafer@outlook.com>
2021-04-09 17:21:45 +0800

    chore: 修复启动问题

--- src/router.js
+++ src/router.js
@@ -4,67 +4,75 @@ import store from './store';
 
 Vue.use(VueRouter);
 
-const routes = [{
+const routes = [
+  {
     path: '/',
-    redirect: '/projects'
-},{
+    redirect: '/projects',
+  },
+  {
     path: '/projects',
     name: 'projects',
-    component: () => import('@/pages/projects/index')
-}, {
+    component: () => import('@/pages/projects/index'),
+  },
+  {
     path: '/dashboard',
     name: 'dashboard',
     component: () => import('@/pages/dashboard/index'),
     beforeEnter: (to, from, next) => {
-        store.dispatch('getWorkingDirectory').then(res => {
-            if (res) next();
-            else next('/projects');
-        });
+      store.dispatch('getWorkingDirectory').then((res) => {
+        if (res) next();
+        else next('/projects');
+      });
     },
     children: [
-        {
-            path: 'overview',
-            name: 'overview',
-            component: () => import('@/pages/home')
-        },
-        {
-            path: 'materials',
-            name: 'materials',
-            component: () => import('@/pages/materials'),
-            children: [{
-                path: 'blocks',
-                name: 'blocks',
-                component: () => import('@/pages/blocks')
-            }, {
-                path: 'components',
-                name: 'components',
-                component: () => import('@/pages/components')
-            }]
-        },
-        {
+      {
+        path: 'overview',
+        name: 'overview',
+        component: () => import('@/pages/home/index'),
+      },
+      {
+        path: 'materials',
+        name: 'materials',
+        component: () => import('@/pages/materials'),
+        children: [
+          {
             path: 'blocks',
             name: 'blocks',
-            component: () => import('@/pages/blocks')
-        }, {
-            path: 'setting',
-            name: 'setting',
-            component: () => import('@/pages/setting')
-        },
-        {
-            path: 'tasks',
-            name: 'tasks',
-            component: () => import('@/pages/tasks')
-        },
-        {
-            path: 'dependence',
-            name: 'dependence',
-            component: () => import('@/pages/dependence')
-        }
-    ]
-}];
+            component: () => import('@/pages/blocks'),
+          },
+          {
+            path: 'components',
+            name: 'components',
+            component: () => import('@/pages/components'),
+          },
+        ],
+      },
+      {
+        path: 'blocks',
+        name: 'blocks',
+        component: () => import('@/pages/blocks'),
+      },
+      {
+        path: 'setting',
+        name: 'setting',
+        component: () => import('@/pages/setting'),
+      },
+      {
+        path: 'tasks',
+        name: 'tasks',
+        component: () => import('@/pages/tasks'),
+      },
+      {
+        path: 'dependence',
+        name: 'dependence',
+        component: () => import('@/pages/dependence'),
+      },
+    ],
+  },
+];
 
 const router = new VueRouter({
-    routes
+  routes,
 });
 
 export default router;
--- vue.config.js
+++ vue.config.js
@@ -1,37 +1,42 @@
 const path = require('path');
-const Mode = require('frontmatter-markdown-loader/mode')
+const Mode = require('frontmatter-markdown-loader/mode');
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
 
 module.exports = {
-    chainWebpack: config => {
-        config.module
-          .rule('markdown')
-          .test(/\.md$/)
-          .use('frontmatter-markdown-loader')
-            .loader('frontmatter-markdown-loader')
-            .tap(options => {
-              return {
-                mode: [Mode.VUE_COMPONENT]
-              }
-            })
+  chainWebpack: (config) => {
+    config.module
+      .rule('markdown')
+      .test(/\.md$/)
+      .use('frontmatter-markdown-loader')
+      .loader('frontmatter-markdown-loader')
+      .tap((options) => {
+        return {
+          mode: [Mode.VUE_COMPONENT],
+        };
+      });
+
+    config.resolve.alias.set('@$', resolve('src'));
+  },
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.(css|less)$/,
+          use: [
+            {
+              loader: 'style-resources-loader',
+              options: {
+                patterns: [
+                  path.resolve(__dirname, './src/style/variables.less'),
+                  path.resolve(__dirname, './src/style/mixin.less'),
+                ],
+              },
+            },
+          ],
+        },
+      ],
     },
-    configureWebpack: {
-        module: {
-            rules: [
-                {
-                    test: /\.(css|less)$/,
-                    use: [
-                        {
-                            loader: 'style-resources-loader',
-                            options: {
-                                patterns: [
-                                    path.resolve(__dirname, './src/style/variables.less'),
-                                    path.resolve(__dirname, './src/style/mixin.less')
-                                ]
-                            }
-                        }
-                    ]
-                }
-            ]
-        }
-    }
+  },
 };
