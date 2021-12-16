fd0b1f4679af5ba170d1050b0341dee05344dcce
giscafer <giscafer@outlook.com>
2021-12-16 16:37:45 +0800

    feat: add react materials

--- src/const.js
+++ src/const.js
@@ -2,7 +2,7 @@ export const appName = 'roothub'
 export const DOC_URL = 'https://roothome.yuque.com/docs/share/8818727b-bc31-42e8-96a9-627c5df4a241'
 export const ISSUES = 'https://github.com/RootLinkFE/roothub/issues'
 export const MATERIALS_LIST = [
-  { title: 'React.js 组件库', url: 'http://comments.leekhub.com' },
-  { title: 'Vue.js 物料', url: 'http://vue.leekhub.com' },
-  { title: 'React.js 物料（WIP…）' }
+  { title: 'React.js 组件库', url: 'http://components.leekhub.com' },
+  { title: 'React.js 物料', url: 'http://materials-react.leekhub.com' },
+  { title: 'Vue.js 物料', url: 'http://vue.leekhub.com' }
 ]
