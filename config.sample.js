f61b07f013a5dc729c718232ed3d13d43b4252a4
suxiaoxia <xiaoxia.su@rootcloud.com>
2022-01-10 15:45:31 +0800

    chore：补充配置

--- config.sample.js
+++ config.sample.js
@@ -1,4 +1,5 @@
 module.exports = {
   backendServerUrl: 'http://(your ip):8111/api', // 生产后端服务地址（本地开发不需要配置）
-  weworkKey: 'd57bb8ec-34f5-leekfund-1111-e6ca908d37e3' // 企业微信机器人 key
+  weworkKey: 'd57bb8ec-34f5-leekfund-1111-e6ca908d37e3', // 企业微信机器人 key
+  weworkKey_dev: 'd57bb8ec-34f5-leekfund-1111-e6ca908d37e3' //dev环境 企业微信机器人 key
 }
